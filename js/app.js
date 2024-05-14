36b7ace68574c8344c55efb90dfefc74311c7503
lhinds86 <leonhinds1986@gmail.com>
2024-05-14 11:52:40 -0400

    -Finished some CSS styling -Started working on Javascript

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 console.log('working')
 
-const playBtn = document.getElementById('playBtn')
-const stopBtn = document.getElementById('stopBtn')
-const skipPreBtn = document.getElementById('skipPreBtn')
-const skipNextBtn = document.getElementById('skipNextBtn')
+const playBtn = document.getElementById('play')
+const prevBtn = document.getElementById('prev')
+const NextBtn = document.getElementById('next')
+const audio = document.getElementById('audio')
+const progress = document.querySelector('.progress')
+const progressContainer = document.querySelector('.progress-container')
+const title = document.getElementById('title')
+const cover = document.getElementById('cover')
 
 console.log(playBtn)
 
